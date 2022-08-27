2d594006bffedb51b5b92fab5a1b1bd32c7543d9
sha7hid <shahidshafadshahad@gmail.com>
2022-08-27 07:58:47 +0530

    create intro.jsx and css

--- recs/src/App.js
+++ recs/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Intro from './Components/Intro/Intro';
 
 function App() {
   return (
     <div className="App">
-hello
+<Intro/>
     </div>
   );
 }
---
+++ recs/src/Components/Intro/Intro.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Intro() {
+  return (
+  <div className="intro-main">
+<div className="intro-text">
+    Select your movie
+</div>
+  </div>
+  )
+}
+
+export default Intro
