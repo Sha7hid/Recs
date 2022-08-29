a3c3223cf2e408764518367815961dd1a0d01622
sha7hid <shahidshafadshahad@gmail.com>
2022-08-29 18:42:42 +0530

    update

--- recs/src/Components/Row/Row.js
+++ recs/src/Components/Row/Row.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-import axios from 'axios'
+import axios from 'axios';
 function Row() {
   const [users, setUsers] = useState([])
   const fetchData = () => {
