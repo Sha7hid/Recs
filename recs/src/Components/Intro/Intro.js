4d30b5ee51ffd26cb530cb0dad7f4ba4c7a39c0f
sha7hid <shahidshafadshahad@gmail.com>
2022-09-02 19:33:56 +0530

    update

--- recs/src/Components/Intro/Intro.js
+++ recs/src/Components/Intro/Intro.js
@@ -4,7 +4,8 @@ function Intro() {
   return (
   <div className="Intro-main">
 <div className="Intro-text">
-   <span>Select your movie</span> 
+  <h2>Recs</h2>
+   <span>Get recommendation based on the movie you select</span> 
 </div>
   </div>
   )
