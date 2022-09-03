6659156b66b3c6f457f5bd41465878ac6ae5da2a
sha7hid <shahidshafadshahad@gmail.com>
2022-09-03 19:20:05 +0530

    create search.jsx and css

--- recs/src/App.js
+++ recs/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Intro from './Components/Intro/Intro';
 import Row from './Components/Row/Row';
+import Search from './Components/Search/Search';
 
 function App() {
   return (
     <div className="App">
 <Intro/>
+<Search/>
 <Row/>
     </div>
   );
