d78c382b696d1ba451a7869d3a600aba9feeb812
sha7hid <shahidshafadshahad@gmail.com>
2022-09-05 11:02:19 +0530

    update

--- recs/src/Components/Search/Search.jsx
+++ recs/src/Components/Search/Search.jsx
@@ -1,18 +1,17 @@
 import React, { useState, useEffect } from "react"
 
 function Search() {
-    const [mov, setMovie, results] = useState([])
+    const [movie, setMovie,] = useState([])
 
     const fetchData = e => {
       const query = e.target.value
-      fetch(`
-      https://api.themoviedb.org/3/search/movie?api_key=57e19e5c44a33653ce6bfc54743c9e2e&language=en-US&query=${query}&page=1&include_adult=false`)
+      fetch(`https://api.themoviedb.org/3/search/movie?api_key=57e19e5c44a33653ce6bfc54743c9e2e&language=en-US&query=${query}&page=1&include_adult=false`)
         .then(response => {
           return response.json()
         })
         .then(data => {
           setMovie(data.results)
-          console.log(data.results)
+          
         })
     }
   
@@ -21,9 +20,8 @@ function Search() {
         <input onChange={fetchData} label="Search User" />
         {setMovie.length > 0 && (
             <ul>
-        {mov.map(results => {
-            
-    <li>{results.original_title}</li>
+ {movie.map(results => {
+    return (<li>{results.original_title}</li>)
 })}
 </ul>
          )}
