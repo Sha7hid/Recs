72bedf70c78cbd791b8494b6ef04541716c60ce1
sha7hid <shahidshafadshahad@gmail.com>
2022-09-05 12:19:41 +0530

    u

--- recs/src/App.js
+++ recs/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Intro from './Components/Intro/Intro';
 
+
 import Row from './Components/Row/Row';
 import Search from './Components/Search/Search';
 
 function App() {
   return (
     <div className="App">
+   
 <Intro/>
 <Search/>
 <Row/>
