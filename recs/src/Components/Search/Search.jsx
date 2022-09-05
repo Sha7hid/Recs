a99bd205639ad59f67b1ad4b9a8c80d223d43e1a
sha7hid <shahidshafadshahad@gmail.com>
2022-09-05 09:33:39 +0530

    update

--- recs/src/Components/Search/Search.jsx
+++ recs/src/Components/Search/Search.jsx
@@ -1,30 +1,33 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
 function Search() {
-    const [users, setUsers] = useState([])
+    const [mov, setMovie, results] = useState([])
 
     const fetchData = e => {
       const query = e.target.value
-      fetch(`https://jsonplaceholder.typicode.com/users?q=${query}`)
+      fetch(`
+      https://api.themoviedb.org/3/search/movie?api_key=57e19e5c44a33653ce6bfc54743c9e2e&language=en-US&query=${query}&page=1&include_adult=false`)
         .then(response => {
           return response.json()
         })
         .then(data => {
-          setUsers(data)
+          setMovie(data.results)
+          console.log(data.results)
         })
     }
   
     return (
       <div>
         <input onChange={fetchData} label="Search User" />
-        {users.length > 0 && (
-          <ul>
-            {users.map(user => (
-              <li key={user.id}>{user.name}</li>
-            ))}
-          </ul>
-        )}
-      </div>
+        {setMovie.length > 0 && (
+            <ul>
+        {mov.map(results => {
+            
+    <li>{results.original_title}</li>
+})}
+</ul>
+         )}
+    </div>
     )
 }
 
